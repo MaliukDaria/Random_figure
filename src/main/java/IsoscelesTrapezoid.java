c2228a3737ebdadd19c3b627d92f18f57530173b
MaliukDaria <maliuk.dasha@gmail.com>
2020-07-28 19:17:02 +0300

    created abstract class Figure and its heirs: Circle, IsoscelesTrapezoid,Squere,Triangle

---
+++ src/main/java/IsoscelesTrapezoid.java
@@ -0,0 +1,13 @@
+public class Trapezoid extends Figure{
+    double baseA;
+    double baseB;
+    double side;
+
+    void drawFigure() {
+        System.out.println((char)(9674));
+    }
+
+    double calculateArea() {
+        return 0;
+    }
+}
